b6349a095efddfe2ade2996db7bcc292bdd381fd
vlados <test@test.test>
2016-06-05 15:19:46 +0300

    Adding task for integration test. It will launch main method.

--- src/main/java/com/test/HelloMessage.java
+++ src/main/java/com/test/HelloMessage.java
@@ -5,7 +5,7 @@ package com.test;
  */
 public class HelloMessage {
     public static void main(String[] args) {
-        System.out.println(new HelloMessage().getMessage());
+        System.out.println(new HelloMessage().getMessage() + " test");
     }
 
     public String getMessage() {
